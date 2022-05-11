8dd28650f83231a6957ee0fc7933681fba234291
tedthetrumpet <tedthetrumpet@gmail.com>
2022-05-11 11:35:41 +0100

    Update trainpla.js

--- trainpla.js
+++ trainpla.js
@@ -9,8 +9,10 @@ osc(30,[0.05,-0.03].smooth(),8).diff(noise(0.001))
 
 /////////////// stereo play
 
-voronoi(3,0.5)
+voronoi([9,3,11].smooth(2).fast(0.1),0.5)
 .colorama([0.7,0.2,0.3].smooth())
 .modulateScale(osc(30,0.05,[0.7,1]))
-.modulate(o0)
+//.modulate(o0)
+//.modulateRotate(o0,3)
+.modulateScale(o0, -0.1)
 .out()
